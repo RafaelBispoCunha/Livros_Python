0af1580b6949393ca3ea321a90bb6b2dc78854ee
Rafael Bispo <rafael_pbc@yahoo.com.br>
2020-01-10 13:53:07 -0300

    Primeiros exercícios do livro.

---
+++ Livro_Curso_Intensivo_de_Python/capitulo_02/exercicio_01.py
@@ -0,0 +1,9 @@
+'''
+Mensagem simples: Armazene uma mensagem em uma variavel e, em seguida exiba essa mensagem.
+Pagina 54.
+
+Rafael Bispo
+10/01/2020
+'''
+mensagem = "Hello World"
+print(mensagem)
---
+++ Livro_Curso_Intensivo_de_Python/capitulo_02/exercicio_02.py
@@ -0,0 +1,13 @@
+'''
+Mensagem simples: Armazene uma mensagem em uma variavel e, em seguida exiba essa mensagem.
+Então altere o valor de sua variavel para uma nova mensagem e mostre essa nova mensagem.
+Pagina 54.
+
+Rafael Bispo
+10/01/2020
+'''
+mensagem = "Hello World"
+print(mensagem)
+mensagem = "Olá, Mundo"
+print(mensagem)
+
